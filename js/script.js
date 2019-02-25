1212c71aa0860d2237a9b5e7cbede5677e566751
Keanneyi Xelhua <kxelhua@tywls.org>
2019-02-25 03:48:27 +0000

    jquery css

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ var songs = ["Sea","Dusk Till Dawn","Let You Down","Heartbreak Story"];
 var imageLinks=["https://upload.wikimedia.org/wikipedia/en/6/62/Ly-her.jpg","https://m.media-amazon.com/images/I/81jbeNY9nxL._SS500_.jpg", "https://streamd.hitparade.ch/cdimages/nf-let_you_down_s.jpg", "https://m.media-amazon.com/images/I/81kj+H5gt2L._SS500_.jpg"];
 var artists=["BTS","Zayn Malik","NF","The Wanted"];
 var songLengths=["5:16","4:27","3:32","2:57"];
-var songLinks=["","","",""];
+var songLinks=["https://www.youtube.com/watch?v=z4gSWMEz4Nc","https://www.youtube.com/watch?v=tt2k8PGm-TI","https://www.youtube.com/watch?v=fbHbTBP_u7U","https://www.youtube.com/watch?v=vWZQZxqaLt8"];
 
 
 function displaySongInfo(){
@@ -26,22 +26,25 @@ function displaySongInfo(){
         $("#lengths").append('<p>'+ songLength + '</p>')
     })
      songLinks.forEach(function(songLink){
-        $("#links").append('<p>'+ songLink + '</p>')
+        $("#links").append('<a href="'+ songLink + '">Click here! </a>')
     })
 }
 
 function emptySongInfo(){
     $("#songs").empty();
-    // Use jQuery to empty all of the remaining divs
-
-
+    $("#artists").empty();
+    $("#images").empty();
+    $("#lengths").empty();
+    $("#links").empty();
 }
 
 
 function addSongInfo(){
-    // BELOW write the code to add new items to each of the arrays.
-
-
+    songs.push($("#song").val());
+    artists.push($("#artist").val());
+    imageLinks.push($("#image").val());
+    songLengths.push($("#length").val());
+    songLinks.push($("#link").val());
 }
 
 $("#add").click(function() {
